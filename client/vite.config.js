585f1b509c9068317c3ee08f4e32364566b912f0
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2024-08-04 00:03:18 +0530

    Ensure Vite build configuration and update Vercel settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8000", // Proxy API requests to the server
+      "/api": "http://localhost:8000",
     },
   },
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure this is set to 'dist'
+    outDir: "dist",
   },
 });
