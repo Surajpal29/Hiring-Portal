c47e96b72431a87a5a8e15a8053a3f8d7498e31a
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2024-08-04 11:10:43 +0530

    some canges

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "http://localhost:8000",
-    },
-  },
   plugins: [react()],
   build: {
     outDir: "dist",
