cec1dc4b5318165a2cc1c196969ac6f415953f04
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2024-08-04 12:16:24 +0530

    c

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // Change from '@vitejs/plugin-react-swc' to '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
